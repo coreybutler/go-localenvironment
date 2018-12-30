4d5db350975e238afe7d0c9bc38afb0dc2662039
Sam Mulube <sam@umbrellium.co.uk>
2018-12-30 01:42:11 +0000

    Have test file create the file we then test for

--- localenvironment_test.go
+++ localenvironment_test.go
@@ -1,9 +1,39 @@
 package localenvironment
 
-import "testing"
-import "os"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func CreateEnvFile(t *testing.T, vars map[string]string) {
+	t.Helper()
+	b, err := json.Marshal(vars)
+	if err != nil {
+		t.Fatalf("Unexpected error marshalling vars: '%v'", err)
+	}
+	err = ioutil.WriteFile("env.json", b, 0644)
+	if err != nil {
+		t.Fatalf("Unexpected error writing file: '%v'", err)
+	}
+}
+
+func DeleteEnvFile(t *testing.T) {
+	t.Helper()
+	err := os.Remove("env.json")
+	if err != nil {
+		t.Fatalf("Unexpected error deleting file: '%v'", err)
+	}
+}
 
 func TestLocalEnvironment(t *testing.T) {
+	CreateEnvFile(t, map[string]string{
+		"TEST": "Success",
+	})
+
+	defer DeleteEnvFile(t)
+
 	err := Apply()
 	if err != nil {
 		t.Fatalf("Unexpected error received from Apply: '%v'", err)
@@ -24,5 +54,12 @@ func TestLocalEnvironment(t *testing.T) {
 	clearedValue := os.Getenv("TEST")
 	if clearedValue != "" {
 		t.Errorf("Unexpected value received for TEST after Clear: '%s'", clearedValue)
+	}
+}
+
+func TestHandlesMissingFile(t *testing.T) {
+	err := Apply()
+	if err != nil {
+		t.Fatalf("Unexpected error received from Apply: '%v'", err)
 	}
 }
